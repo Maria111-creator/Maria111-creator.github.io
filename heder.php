f32b0910ec30af1ff422fda0a5614b27270ef036
Maria111-creator <58450046+Maria111-creator@users.noreply.github.com>
2019-12-03 03:06:53 +0200

    First Commit

---
+++ heder.php
@@ -0,0 +1,15 @@
+﻿<html>
+<head>
+<title> Програмування для кожного </title>
+ <link rel="shortcut icon" href="ico.ico"  type="image/x-icon">
+ <link rel="stylesheet" href="style.css" type="text/css" media="all">
+ </head>
+<body>
+<div class="block2">
+			<ul class="css-menu-1">
+	    <li><a href="index.php"> Головна  сторінка </a></li>
+		        <li><a href="mova.php"> Обираємо  мову </a></li>
+        <li><a href="video.php"> Відео  уроки </a></li>
+		<li><a href="contacty.php"> Наші  контакти </a></li>
+			</ul>		</div>
+
